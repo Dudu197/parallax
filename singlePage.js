9d8bc15d70724c4a66ea260a8d89fa33e991f32e
Dudu197 <cadu97@gmail.com>
2015-03-30 11:29:25 -0300

    Update singlePage.js

--- singlePage.js
+++ singlePage.js
@@ -11,38 +11,67 @@ var afterLoad = function(){
   var lastScrollTop = 0;
   
   $window.bind("scroll resize", function() {
-  
-    
-    var page = (window.location + "").substring((window.location + "").lastIndexOf("/") + 1);
-    
-    if(page == ""){
-        page = "index";
-    }
-    index = order.indexOf("#" + page);
-    
-    var st = $(this).scrollTop();
-    if (st >= lastScrollTop){
-        // downscroll code
-        var fixo = index + 1;
-        var relativo = index;
-    } else {
-        // upscroll code
-        var fixo = index + 1;
-        var relativo = index;
-    }
-    lastScrollTop = st;
-    
-    
-    $(".frame"+order[fixo]).css("position", "fixed");
-    $(".frame"+order[fixo]).css("top", "0");
-    $(".frame"+order[fixo]).css("z-index", "-1");
-    $(".frame"+order[fixo + 1]).css("z-index", "-2");
-    
-    $(".frame"+order[relativo]).css("position", "relative");
-    $(".frame"+order[relativo]).css("top", tops[index]);
-    $(".frame"+order[relativo]).css("z-index", "auto");
-  
-  });
+
+        var lastPage = "";
+
+        var page = (window.location + "").substring((window.location + "").lastIndexOf("/") + 1);
+
+        if(page == ""){
+            page = "index";
+        }
+        index = order.indexOf("#" + page);
+
+        var st = $(this).scrollTop();
+        if (st >= lastScrollTop){
+            // downscroll code
+            var fixo = index + 1;
+            var relativo = index;
+        } else {
+            // upscroll code
+            var fixo = index + 1;
+            var relativo = index;
+        }
+        lastScrollTop = st;
+
+
+        $(".frame"+order[fixo]).css("position", "fixed");
+        $(".frame"+order[fixo]).css("top", "0");
+        $(".frame"+order[fixo]).css("z-index", "-1");
+        $(".frame"+order[fixo + 1]).css("z-index", "-2");
+
+        $(".frame"+order[relativo]).css("position", "relative");
+        $(".frame"+order[relativo]).css("top", tops[index]);
+        $(".frame"+order[relativo]).css("z-index", "auto");
+        if(((window.location + "").lastIndexOf("/") + mais) === ((window.location + "").length - 1 )){
+        	$Nav.css({
+                position: 'absolute',
+                top: (parseInt(maxTop) + 50)
+            });
+            window.navFixed = 1;
+        }else{
+        	$Nav.css({
+                position: 'fixed',
+                top: '0%'
+            });
+            window.navFixed = 1;
+        }
+        $("nav .ativo").removeClass("ativo");
+
+        pagina = (window.location + "").substring((window.location + "").lastIndexOf("/") + 1);
+        if((window.location + "").indexOf("en_") == -1){
+            if(pagina == ""){
+                $('a[data-frame="#index"]').addClass("ativo");
+            }else{
+                $('a[data-frame="#'+pagina + '"]').addClass("ativo");
+            }
+        }else{
+            if(pagina == ""){
+                $('a[data-frame="#en_index"]').addClass("ativo");
+            }else{
+                $('a[data-frame="#'+pagina + '"]').addClass("ativo");
+            }
+        }
+    }).scroll();
   
 }
 
